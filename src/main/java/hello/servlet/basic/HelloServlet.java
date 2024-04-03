2b5d9822d229a1b01c5218214ee5fe2823effca0
dpfls0922 <dpfls0922@naver.com>
2024-04-03 15:31:44 +0900

    [Feat] response, request 객체 생성 및 데이터 주입

---
+++ src/main/java/hello/servlet/basic/HelloServlet.java
@@ -0,0 +1,26 @@
+package hello.servlet.basic;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+@WebServlet(name = "helloServlet", urlPatterns = "/hello")
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        System.out.println("request = " + request);
+        System.out.println("response = " + response);
+
+        String username = request.getParameter("username");
+        System.out.println("username = " + username);
+
+        response.setContentType("text/plain");              // HTTP header에 데이터 타입 넣기
+        response.setCharacterEncoding("utf-8");             // HTTP header에 데이터 타입 넣기
+        response.getWriter().write("hello " + username); // HTTP body에 데이터 넣기
+    }
+}
