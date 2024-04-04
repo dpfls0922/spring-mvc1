27cf0b4c3869fd6c0c92e0ce0c3b265e3ead6285
dpfls0922 <dpfls0922@naver.com>
2024-04-04 17:40:56 +0900

    [Feat] MyHandlerAdpater를 상속받고 ControllerV3을 지원하는 핸들러 어댑터 구현

--- src/main/java/hello/servlet/web/frontcontroller/v5/adpater/ControllerV3HandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/adpater/ControllerV3HandlerAdapter.java
@@ -1,2 +1,35 @@
-package hello.servlet.web.frontcontroller.v5.adpater;public class ControllerV3HandlerAdapter {
+package hello.servlet.web.frontcontroller.v5.adpater;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV3HandlerAdapter implements MyHandlerAdapter {
+    @Override
+    public boolean supports(Object handler) {
+        return (handler instanceof ControllerV3);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV3 controller = (ControllerV3) handler;
+
+        Map<String, String> paramMap = createPramMap(request);
+        ModelView mv = controller.process(paramMap);
+
+        return mv;
+    }
+    private static Map<String, String> createPramMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
 }
