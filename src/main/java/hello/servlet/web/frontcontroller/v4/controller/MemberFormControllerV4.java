70a01807bcb0bdfefed61f016f4c45b3bd829e88
dpfls0922 <dpfls0922@naver.com>
2024-04-04 16:37:25 +0900

    [Feat] ControllerV4을 상속받아 논리적인 뷰 이름을 반환하는 컨트롤러 구현

--- src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberFormControllerV4.java
@@ -1,2 +1,11 @@
-package hello.servlet.web.frontcontroller.v4.controller;public class MemberFormControllerV4 {
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import java.util.*;
+
+public class MemberFormControllerV4 implements ControllerV4 {
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        return "new-form";
+    }
 }
--- src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
+++ src/main/java/hello/servlet/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -1,2 +1,19 @@
-package hello.servlet.web.frontcontroller.v4.controller;public class MemberListControllerV4 {
+package hello.servlet.web.frontcontroller.v4.controller;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import java.util.*;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+        model.put("members", members);
+
+        return "members";
+    }
 }
