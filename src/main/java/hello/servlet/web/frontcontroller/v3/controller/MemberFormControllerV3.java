dd5c00e0750e14e7c3711cc6e2d5b01e92eea4c2
dpfls0922 <dpfls0922@naver.com>
2024-04-04 15:59:11 +0900

    [Feat] ControllerV3을 상속받아 논리적인 뷰 이름으로 ModelView 생성하고 반환하는 컨트롤러 구현

--- src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -1,2 +1,11 @@
-package hello.servlet.web.frontcontroller.v3.controller;public class MemberFormControllerV3 {
+package hello.servlet.web.frontcontroller.v3.controller;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v3.ControllerV3;
+import java.util.*;
+public class MemberFormControllerV3 implements ControllerV3 {
+    @Override
+    public ModelView process(Map<String, String> paramMap) {
+        return new ModelView("new-form");
+    }
 }
